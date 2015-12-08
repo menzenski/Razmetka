13933482a3f03df1d647d06082785159fc146ae8
Matt Menzenski <matt.menzenski@gmail.com>
2015-12-08 07:29:59 -0600

    remove older directories from remote repo

--- segtester/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-__author__ = 'Matt Menzenski'
-__author_email__ = 'matt.menzenski@gmail.com'
-__version__ = '0.0.1'
-
-def get_version():
-    return __version__
